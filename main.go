24641de378170072e15ff3a6cb818645979167d7
oavdonin <9444556+oavdonin@users.noreply.github.com>
2025-03-28 12:32:33 +0100

    Completed 1_8 excercise

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import (
 	"net/http"
 )
 
-// type webServer struct{}
-
-// func (webServer) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
 func main() {
 	const port = "8080"
+	const healthcheck_path = "/healthz"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc(healthcheck_path, healthcheckHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
