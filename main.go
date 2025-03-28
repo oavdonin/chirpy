3c18879a8e7c65e8e9182fab4d4a5d4e6425dbb3
oavdonin <9444556+oavdonin@users.noreply.github.com>
2025-03-28 16:29:28 +0100

    Completed excercise 2_1

--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
-func healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
 
 func main() {
 	const port = "8080"
-	const healthcheck_path = "/healthz"
+
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc(healthcheck_path, healthcheckHandler)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", healthcheckHandler)
+	mux.HandleFunc("/metrics", cfg.showMetricsHandler)
+	mux.HandleFunc("/reset", cfg.resetMetricsHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -26,4 +31,29 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+func (cfg *apiConfig) showMetricsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Hits: " + strconv.FormatInt(int64(cfg.fileserverHits.Load()), 10)))
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
